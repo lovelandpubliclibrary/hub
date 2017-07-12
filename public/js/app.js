b71ffc4a8f7091a2b090b9f8ee1df35f152779a7
Kevin Briggs <kevin.briggs@cityofloveland.org>
2017-07-12 12:12:03 -0600

    Environment updates to enable Bootstrap glyphicons in Larvel 5.4 (laravel-mix)

--- public/js/app.js
+++ public/js/app.js
@@ -31664,7 +31664,7 @@ var Component = __webpack_require__(35)(
   /* cssModules */
   null
 )
-Component.options.__file = "/Users/kevin/Sites/wiki/resources/assets/js/components/Example.vue"
+Component.options.__file = "/app/resources/assets/js/components/Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -31675,9 +31675,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-1efcedd4", Component.options)
+    hotAPI.createRecord("data-v-10a3239e", Component.options)
   } else {
-    hotAPI.reload("data-v-1efcedd4", Component.options)
+    hotAPI.reload("data-v-10a3239e", Component.options)
   }
 })()}
 
@@ -31766,7 +31766,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-1efcedd4", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-10a3239e", module.exports)
   }
 }
 
