2b2bac859239d7816ebef302680bd0362f7ae097
Kevin Briggs <kevin.briggs@cityofloveland.org>
2017-07-17 15:12:56 -0600

    Update database seeding and remove unnecessary .gitignore for database directory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(IncidentsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(StatusesTableSeeder::class);
     }
 }
 
