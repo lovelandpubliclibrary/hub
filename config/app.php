7d2534eaf882df39ab778299a71563f0011d6e17
Kevin Briggs <kevin.briggs@cityofloveland.org>
2017-07-18 18:14:25 -0600

    Updated from Wiki to Repository

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Loveland Public Library Wiki'),
+    'name' => env('APP_NAME', 'LPL Repository'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'LPL Wiki') }}</title>
+    <title>{{ config('app.name', 'LPL Repository') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
