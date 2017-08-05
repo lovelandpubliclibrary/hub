7af5191207c51a244a89960758139ebe0279af3a
Kevin Briggs <kevin.briggs@cityofloveland.org>
2017-08-05 11:27:42 -0600

    Add 'required' asterisk to Incident creation form

--- resources/views/incidents/create.blade.php
+++ resources/views/incidents/create.blade.php
@@ -26,13 +26,13 @@
 
 			<div class="panel-body">
 				{{ Form::open(['action' => 'IncidentController@store', 'files' => true]) }}
-					<div class="form-group">
-						{{ Form::label('title', 'Title:') }}
+					<div class="form-group required">
+						{{ Form::label('title', 'Title:', ['class' => 'control-label']) }}
 						{{ Form::text('title', null, ['class' => 'form-control', 'required' => 'required']) }}
 					</div>
 					
-					<div class="form-group">
-					    {{ Form::label('date', 'Date of Incident:') }}
+					<div class="form-group required">
+					    {{ Form::label('date', 'Date of Incident:', ['class' => 'control-label']) }}
 					    {{ Form::date('date', \Carbon\Carbon::now(), ['class' => 'form-control', 'required' => 'required']) }}
 				  	</div>
 
@@ -51,8 +51,8 @@
 						{{ Form::text('patron_description', null, ['class' => 'form-control']) }}
 					</div>
 
-					<div class="form-group">
-						{{ Form::label('description', 'Describe the Incident:') }}
+					<div class="form-group required">
+						{{ Form::label('description', 'Describe the Incident:', ['class' => 'control-label']) }}
 						{{ Form::textarea('description', null,
 										  ['class' => 'form-control', 'rows' => '6', 'required' => 'required']) }}
 					</div>
