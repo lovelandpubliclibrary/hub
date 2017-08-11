cf28a8b83dd39bce0df1ceb3d18ec9e53108b9a1
Kevin Briggs <kevin.briggs@cityofloveland.org>
2017-08-11 12:37:57 -0600

    Implement simpler breadcrumbs for screens < 768px

--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -1,4 +1,5 @@
 @if (isset($breadcrumbs))
+	{{-- display full breadcrumbs for larger screens --}}
 	<ul class="breadcrumb hidden-xs">
 		@foreach ($breadcrumbs as $breadcrumb)
 			<li>
@@ -8,4 +9,18 @@
 			</li>
 		@endforeach
 	</ul>
+
+	{{-- display simple breadcrumbs for mobile screens --}}
+	<div class="breadcrumb visible-xs">
+		@if (!preg_match('/' . Request::segment(1) . '\/?$/', url()->current()))
+			<a href="{{ route(Request::segment(1)) }}">
+				<< Back to {{ ucwords(Request::segment(1)) }}
+			</a>
+		@else
+			<a href="{{ route('home') }}">
+				<< Back to Home
+			</a>
+		@endif
+	</div>
+
 @endif
