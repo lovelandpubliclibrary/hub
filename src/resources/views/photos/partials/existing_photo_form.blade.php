a83350278a20b0621a3dff8eacca64967078a68d
Kevin Briggs <kevinb@psaudio.com>
2019-04-16 20:16:44 -0600

    Fix bug when trying to create columns of photos and there is only one in the database

--- src/resources/views/photos/partials/existing_photo_form.blade.php
+++ src/resources/views/photos/partials/existing_photo_form.blade.php
@@ -2,20 +2,29 @@
 
 <div class="form-group required">
 	<div class="row">
-		@foreach ($photoColumns as $column)
-			<div class="column">
-				@foreach ($column as $photo)
-					<button class="thumbnail selectExistingPhoto"
-						data-photo-id="{{ $photo->id }}">
-						<img src="{{ asset('storage/photos/' . $photo->filename) }}">
-						<div>
-							Photo #{{ $photo->id }}
-						</div>
-					</button>
-				@endforeach
-			</div>
-			
-		@endforeach
+		@if ($photoColumns->count() > 1)
+			@foreach ($photoColumns as $column)
+				<div class="column">
+					@foreach ($column as $photo)
+						<button class="thumbnail selectExistingPhoto"
+							data-photo-id="{{ $photo->id }}">
+							<img src="{{ asset('storage/photos/' . $photo->filename) }}">
+							<div>
+								Photo #{{ $photo->id }}
+							</div>
+						</button>
+					@endforeach
+				</div>
+			@endforeach
+		@else
+			<button class="thumbnail selectExistingPhoto"
+				data-photo-id="{{ $photoColumns->first()->id }}">
+				<img src="{{ asset('storage/photos/' . $photoColumns->first()->filename) }}">
+				<div>
+					Photo #{{ $photoColumns->first()->id }}
+				</div>
+			</button>
+		@endif
 	</div>
 </div>
 
