e141a3d0ec07b4f6775ff8b9151ec53098613274
Kevin Briggs <redisforlosers@gmail.com>
2019-04-30 19:06:13 -0600

    Remove extraneous success message when saving a new patron

--- src/resources/views/patrons/show.blade.php
+++ src/resources/views/patrons/show.blade.php
@@ -3,12 +3,7 @@
 @section('content')
 	<div id="patrons">
 		@include('layouts.breadcrumbs')
-{{-- {{dd($patron)}} --}}
-		@if(Session::has('success_message'))
-			<div class="alert alert-success">
-				{{ Session::get('success_message') }}
-			</div>
-		@endif
+
 
 		@if (count($errors) > 0)
 			<div class="alert alert-danger">
